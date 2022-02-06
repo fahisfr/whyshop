4d03edaf9cca94e4e3193910b1670045be708fb9
fahisfr <lastorange123@gmail.com>
2022-02-06 16:11:08 +0530

    change jwt route to auth and cliend side axios interceptors.response setup for reauth(refreshtoken)

--- cliend/src/Axios.js
+++ cliend/src/Axios.js
@@ -1,14 +1,36 @@
 import axios from 'axios';
 
 
-const baseURL = 'https://frbots.com/';
+const baseURL = 'http://localhost:4000/';
 
 export const ImagePath = (imageId) => `${baseURL}images/${imageId}.jpg`;
 
 const instance = axios.create({
-    baseURL: 'https://frbots.com/api',
-    headers: { 'authorization': localStorage.getItem('accesstoken') }
-},);
+    baseURL: 'http://localhost:4000/api/',
+    headers: {
+        'authorization': localStorage.getItem('accesstoken'),
+    },
+    withCredentials: true
+   
+});
+instance.interceptors.response.use(
+    response => response,
+    async error => {
+        if (error.response.status === 401) {
+            try {
+                localStorage.removeItem('accesstoken')
+                const { data } = await instance.put('/auth/refreshtoken', { withCredentials: true })
+                localStorage.setItem('accesstoken', data.accesstoken)
+                window.location.reload('/')
+            } catch (_error) {
+                localStorage.removeItem('accesstoken');
+                return Promise.reject(error)
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 
 
 export default instance
--- server/server.js
+++ server/server.js
@@ -2,23 +2,23 @@ require("dotenv").config();
 
 const express= require('express');
 const app    = express();
-const db     = require('mongoose')
 const cors   = require('cors')
-const morgan = require('morgan')
 const path   = require('path')
-const PORT = process.env.PORT || 4000;
-const fileupload = require('express-fileupload')
-const IsAthu = require('./Middlewares/UserAuthentication')
+const morgan = require('morgan')
+const PORT   = process.env.PORT || 4000;
 const cookieParser = require('cookie-parser')
 const connectDB    = require('./Config/dbConn')
+const fileupload   = require('express-fileupload')
+const corsOptions  = require('./Config/corsOptions')
 const errorHandler = require('./Config/errorHandler')
-
+const IsAthu       = require('./Middlewares/UserAuthentication')
 
 connectDB()
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(fileupload())
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,8 +28,8 @@ app.use('/api/product', require('./Routes/Product'))
 app.use('/api/shop',require('./Routes/Shop'))
 app.use('/api/signup', require('./Routes/Signup'))
 app.use('/api/login', require('./Routes/Login'))
-app.use('/api/authentication', require('./Routes/Authentication'))
-app.use('/api/refreshtoken', require('./Routes/RefreshToken'))
+app.use('/api/auth', require('./Routes/Auth'))
+
 app.use('/api/search-products', require('./Controllers/SearchPrdoucts/SearchProduts'))
 
 app.use('/api/cart',IsAthu, require('./Routes/Cart'))
@@ -42,3 +42,5 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+
